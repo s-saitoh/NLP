eb682005720447c467f58af2f5f549f95bddecb6
s-saitoh <nobiairss@gmail.com>
2017-03-29 10:52:37 +0900

    test08

--- test08.py
+++ test08.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python
 #coding: utf-8
-print(ord("z"))
+str = "Done is Better than Perfect."
+
+def cipher(input):
+    ret = ""
+    for x in input:
+        ret += chr(219-ord(x)) if x.islower() else x
+    return ret
+
+str = cipher(str)
+print(str)
+str = cipher(str)
+print(str)
