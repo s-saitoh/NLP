993ceda9e7e66b54639c159a75778ceefed1fd22
s-saitoh <nobiairss@gmail.com>
2017-03-29 15:32:19 +0900

    test15

---
+++ test15.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding: utf-8
+import sys
+
+with open (sys.argv[1], encoding = "utf-8") as f:
+    lines = f.readlines()
+
+for line in lines[len(lines) - int(sys.argv[2]):]:
+    print(line, end = "")
