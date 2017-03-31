e992877f659ddafd7ed3f8497181e13caada27f9
s-saitoh <nobiairss@gmail.com>
2017-03-31 14:57:33 +0900

    test22

---
+++ test22.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#coding: utf-8
+import gzip
+import json
+import re # 正規表現
+filename = "jawiki-country.json.gz"
+
+def extract_UK():
+    # イギリスの本文を戻り値として取得
+
+    # rtは読み込み専用＆テキストモード（いずれもデフォルト）
+    with gzip.open(filename, "rt", encoding = "utf-8") as data_file:
+        for line in data_file:
+            data_json = json.loads(line)# jsonのデータをロードしてpythonのデータ型に変換
+            if data_json["title"] == "イギリス":# タイトルが「イギリス」の時
+                return data_json["text"]# 本文を戻り値として返す
+
+    raise ValueError("Not Found Article About UK")
+    # タイトルがイギリスでないとき例外処理としてValueErrorを出す
+
+
+# 正規表現のコンパイル、あらかじめ使う正規表現を書き込んでおく
+pattern = re.compile(r'''
+    ^       # 行頭
+    .*      # 任意の文字0文字以上
+    \[\[Category:
+    (       # キャプチャ対象のグループ開始
+    .*?     # 任意の文字0文字以上、非貪欲マッチ（貪欲にすると後半の'|'で始まる装飾を巻き込んでしまう）
+    )       # グループ終了
+    (?:     # キャプチャ対象外のグループ開始
+    \|.*    # '|'に続く0文字以上
+    )?      # グループ終了、0か1回の出現
+    \]\]
+    .*      # 任意の文字0文字以上
+    $       # 行末
+    ''', re.MULTILINE + re.VERBOSE)
+    # MULTILINEは読み込みが複数行あるため、VERBOSEは正規表現中にコメントするため
+
+# 抽出
+result = pattern.findall(extract_UK())#findallはマッチした部分だけを持ってくる
+
+# 結果表示
+for line in result:
+    print(line)
