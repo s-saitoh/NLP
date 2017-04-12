548ff35392b3c6a251a39eea88cffb5490e0237b
s-saitoh <nobiairss@gmail.com>
2017-04-12 17:51:28 +0900

    test34

---
+++ test34.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#coding: utf-8
+import MeCab
+fname = 'neko.txt'
+fname_parsed = 'neko.txt.mecab'
+
+def parse_neko():
+    "「吾輩は猫である」を形態素解析してneko.txt.mecabに保存"
+
+    with open(fname) as data_file, \
+        open(fname_parsed, mode="w") as out_file:
+        mecab = MeCab.Tagger()
+        out_file.write(mecab.parse(data_file.read()))
+
+def neko_lines():
+    """「吾輩は猫である」の形態素解析結果のジェネレータ
+    解析結果を読み込んで各形態素を
+    表層形(surface)
+    基本形(base)
+    品詞(pos)
+    品詞細分類1(pos1)
+    をキーとするマッピング型に格納し、1文を形態素のリストとして表現
+
+    戻り値：
+    1文の各形態素を辞書化したリスト
+    """
+    with open(fname_parsed) as file_parsed:
+
+        morphemes = []# 格納するリスト
+        for line in file_parsed:
+
+            #表層形はtab区切り、それ以外は","区切りでばらす
+            cols = line.split("\t") #tab区切り
+            if(len(cols) < 2):
+                raise StopIteration #区切りがなければ終了
+            res_cols = cols[1].split(",")#それ以外は”,”で区切る
+
+            #辞書作成、リストに追加
+            morpheme = {
+                "surface" : cols[0],
+                "base" : res_cols[6],
+                "pos" : res_cols[0],
+                "pos1" : res_cols[1]
+            }
+            morphemes.append(morpheme)
+
+            #品詞細分類1が句点なら文の終わりと判定
+            if res_cols[1] == '句点':
+                yield morphemes
+                morphemes = []
+
+#形態素解析
+parse_neko()
+
+#1文ずつ辞書のリストを作成し抽出
+list_a_no_b = [] #確認用の出現順リスト
+lines = neko_lines()
+for line in lines:
+    if len(line) > 2: #形態素の数が2つより多くて（3以上）
+        for i in range(1, len(line) - 1):
+            if line[i]['surface'] == 'の'\
+                    and line[i - 1]['pos'] == '名詞'\
+                    and line[i + 1]['pos'] == '名詞': # 表層形がのでかつ前と後の品詞が「の」なら
+                list_a_no_b.append(line[i - 1]['surface'] + 'の' + line[i + 1]['surface'])
+                #出現順リストに追加
+a_no_b = set(list_a_no_b) # 重複除去
+
+#確認しやすいようにnouns_testを使って出現順にソートして表示
+print(str(sorted(a_no_b, key=list_a_no_b.index)).decode("string-escape"))
